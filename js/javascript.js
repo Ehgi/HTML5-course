1b8f445857a4178740f2ec3352cd5f80e3a0cf46
Vladimir Skroban II <42891334+Ehgi@users.noreply.github.com>
2018-11-08 05:52:39 +0700

    another commit
    added some kind of animation, Button hovers are smooth now.
    Added rotation (header slider buttons)
    added scale changing at "Realization" section
    began to make "Recent posts" section.
    Made video works, but yet not good.

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+var video = document.querySelector("#video"),
+    button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+  video.play()
+  video.setAttribute("controls","controls");
+}, false);
+
+$(".video-player-btn").click(function() {
+  $(".video-player-btn").addClass('video-player-btn--hide');
+})
